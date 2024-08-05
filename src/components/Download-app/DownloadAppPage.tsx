66394aa37d41d5026a2792fcaf2c566893f67e5f
tabitabasharat <tabitab632@gmail.com>
2024-08-05 17:13:03 +0500

    download App Page

--- src/components/Download-app/DownloadAppPage.tsx
+++ src/components/Download-app/DownloadAppPage.tsx
@@ -2,28 +2,38 @@ import React from "react";
 import "./DownloadAppPage.css";
 import Image from "next/image";
 import mobileimg from "../../assets/Group-17.png";
-import appstore from "../../assets/image-1-e1711464803461.png"
+import appstore from "../../assets/image-1-e1711464803461.png";
 import googleplay from "../../assets/googleapp.png";
-import natriam from "../../assets/Group-1.png"
+import natriam from "../../assets/Group-1.png";
+import { FadeReveal } from "@/components/animations/FadeReveal";
+import { Reveal } from "@/components/animations/Reveal";
+import elips from "../../assets/Group-16.png";
 
 function DownloadAppPage() {
   return (
-    <div className="main-div-download-app">
-      <div>
-        <Image src={mobileimg} />
-      </div>
-      <div>
-        <div className="flex items-center flex-col text-center">
-          <h2 className="text-4xl mb-5 font-bold">Download</h2>
-       <Image src={natriam} className="mb-5 "/>
-          <h3 className="coming-soon">Coming Soon</h3>
+    <>
+      <div className="main-div-download-app">
+        <Image src={elips} className="elips" />
+        <div className="mobile-img">
+          <Image src={mobileimg} />
         </div>
-        <div className="flex">
-          <Image src={appstore} className="p-2"/>
-          <Image src={googleplay} className="ps-5" />
+        <div className="sceond-div md:w-[75%] lg:w-[50%]">
+          <Reveal y={100} width="100%">
+            <div className="flex items-center flex-col text-center">
+          <Reveal y={100} width="100%">
+              <h2 className=" mb-5 download-text">Download</h2>
+              </Reveal>
+              <Image src={natriam} className="mb-5 green-natiram " />
+              <h3 className="coming-soon">Coming Soon</h3>
+            </div>
+            <div className="flex items-center justify-center">
+              <Image src={appstore} className="appstroe" />
+              <Image src={googleplay} className="ps-5" />
+            </div>
+            </Reveal>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
