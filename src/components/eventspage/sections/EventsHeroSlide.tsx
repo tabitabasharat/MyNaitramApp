08bf921c33bcadfa969ee7e8adc5326321df7d2b
tabitabasharat <tabitab632@gmail.com>
2024-08-07 16:11:30 +0500

    takeover changes

--- src/components/eventspage/sections/EventsHeroSlide.tsx
+++ src/components/eventspage/sections/EventsHeroSlide.tsx
@@ -117,7 +117,7 @@ const EventsHeroSlide = ({
   return (
     <>
       {" "}
-      <div className="relative h-full">
+      {/* <div className="relative h-full " >
         <Image
           style={{ filter: "blur(30px)" }}
           width={1000}
@@ -127,8 +127,9 @@ const EventsHeroSlide = ({
           alt=""
         />
         <div className="absolute inset-0 bg-black/65"></div>
-      </div>
-      <div className="pxpx mx2xl absolute z-[2] top-32 lg:top-1/2 lg:-translate-y-[38%] flex flex-col lg:flex-row gap-6 lg:gap-12 w-full md:w-[70%] lg:w-fit translate-x-1/2 right-1/2">
+      </div> */}
+      {/* <div className="pxpx mx2xl absolute z-[2] top-32 lg:top-1/2 lg:-translate-y-[38%] flex flex-col lg:flex-row gap-6 lg:gap-12 w-full md:w-[70%] lg:w-fit translate-x-1/2 right-1/2"> */}
+      <div>
         <div>
           <div className="hidden flex gap-1 mb-3">
             {top5Events.map((_, index) => (
@@ -141,7 +142,7 @@ const EventsHeroSlide = ({
               ></div>
             ))}
           </div>
-
+          {/* 
           <div
             style={{
               backgroundImage: `url(${"/takeOver.png"})`,
@@ -152,12 +153,12 @@ const EventsHeroSlide = ({
             className="bg-cover bg-no-repeat h-[300px] w-full lg:h-[300px] lg:w-[300px] xl:h-full xl:w-[470px] rounded-lg relative"
           >
             {" "}
-            {/* <div className="bg-white/20 p-[1rem] rounded-full backdrop-blur-lg webkit-header-blur w-fit absolute right-6 bottom-6">
+            <div className="bg-white/20 p-[1rem] rounded-full backdrop-blur-lg webkit-header-blur w-fit absolute right-6 bottom-6">
               <Heart size={23} weight="fill" />
-            </div> */}
-          </div>
-        </div>
-        <div className="lg:w-[70%] ">
+            </div>
+          </div> */}
+          {/* </div> */}
+          {/* <div className="lg:w-[70%] "> */}
           {/* <div className="lg:flex gap-1 hidden mt-4">
             {top5Events.map((_, index) => (
               <div
@@ -170,29 +171,35 @@ const EventsHeroSlide = ({
             ))}
           </div> */}
 
-          <div className="flex gap-[0.35rem] mt-4">
-            <Badge>Party</Badge>
-            <Badge>Invitation</Badge>
-            <Badge>TAKEOVR</Badge>
+          <div className="flex gap-[0.35rem]">
+            <Badge className="lg:text-[12px]">Party</Badge>
+            <Badge className="lg:text-[12px]">Invitation</Badge>
+            <Badge className="lg:text-[12px]">TAKEOVR</Badge>
           </div>
-          <div className="flex gap-[0.35rem] mt-4">
-            <Link target="_blank" href="https://www.instagram.com/takeovr_uk?igsh=ZzVyMmRvcDhob3po">
+          <div className="flex gap-[0.35rem] mt-3">
+            <Link
+              target="_blank"
+              href="https://www.instagram.com/takeovr_uk?igsh=ZzVyMmRvcDhob3po"
+            >
               <InstagramLogo size={30} weight="fill" />
             </Link>
-            <Link target="_blank" href="https://www.tiktok.com/@takeovr_uk?_t=8ocugSj8bKa&_r=1">
+            <Link
+              target="_blank"
+              href="https://www.tiktok.com/@takeovr_uk?_t=8ocugSj8bKa&_r=1"
+            >
               <TiktokLogo size={30} weight="fill" />
             </Link>
           </div>
 
-          <h2 className="text-[28px] lg:w-full lg:text-[40px] xl:text-[55px] font-extrabold leading-[1.2] mt-2">
+          <h2 className="text-[28px] lg:w-full lg:text-[40px] xl:text-[55px] font-extrabold leading-[1.2] my-3">
             {title}
           </h2>
           <div
-            className="flex items-center gap-[4px] cursor-pointer mt-[24px]"
+            className="flex items-center gap-[6px] cursor-pointer"
             onClick={() => AboutToggle()}
           >
             <p className="text-[#13FF7A]">About </p>
-            <Image src={Arrowdown} alt="arrow-icon" />
+            <Image src={Arrowdown} alt="arrow-icon" className="mb-0.5" />
           </div>
           {AboutDrop && (
             <div>
@@ -242,12 +249,12 @@ const EventsHeroSlide = ({
           )}
 
           {/* <p className="text-muted mt-4">Location: {location}</p> */}
-          <p className="text-muted lg:mt-2 mb-6">
+          <p className="text-muted lg:mt-2 mb-5">
             {" "}
             {/* {ConvertDate(eventDate)} {ConvertTime(startTime)} {"-"}{" "}
             {ConvertTime(endTime)} */}
           </p>
-          <BuyTicket event={event} setShowTicket={setShowTicket}/>
+          <BuyTicket event={event} setShowTicket={setShowTicket} />
         </div>
       </div>
     </>
