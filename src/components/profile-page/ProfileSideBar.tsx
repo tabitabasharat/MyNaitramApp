90ebd369eebd765549de444a3c062baea2fe7f2e
tabitabasharat <tabitab632@gmail.com>
2024-08-12 17:55:15 +0500

    user-page

--- src/components/profile-page/ProfileSideBar.tsx
+++ src/components/profile-page/ProfileSideBar.tsx
@@ -1,17 +1,16 @@
-import { cn, shimmer, toBase64 } from '@/lib/utils';
-import Image from 'next/image';
-import GradientBorder from '../ui/gradient-border';
-import AccountSidebarLink from '@/components/reusable-components/AccountSidebarLink';
+import { cn, shimmer, toBase64 } from "@/lib/utils";
+import Image from "next/image";
+import GradientBorder from "../ui/gradient-border";
+import AccountSidebarLink from "@/components/reusable-components/AccountSidebarLink";
 import {
   FileText,
   Headphones,
   Question,
-  Ticket,
-  Translate,
-  Trophy,
   UserGear,
   FileLock,
-} from '@phosphor-icons/react/dist/ssr';
+  Chats,
+  // Deleteaccnt,
+} from "@phosphor-icons/react/dist/ssr";
 
 const ProfileSidebar = ({
   className,
@@ -23,33 +22,33 @@ const ProfileSidebar = ({
   return (
     <div
       className={cn(
-        'sticky top-[9rem] z-[10] h-full bg-black shadow-custom rounded-2xl overflow-hidden',
-        className,
+        "sticky top-[9rem] z-[10] h-full bg-black shadow-custom rounded-2xl overflow-hidden",
+        className
       )}
     >
       <Image
-        src={'/person3.jpg'}
+        src={"/person3.jpg"}
         width={500}
         height={500}
         className="absolute h-[200px] object-cover object-center blur-[20px]"
         placeholder={`data:image/svg+xml;base64,${toBase64(
-          shimmer(1200, 1800),
+          shimmer(1200, 1800)
         )}`}
         alt="DP"
       />
       <div className="absolute inset-0 bg-black/50"></div>
-      <div className="z-[2] relative py-7 px-6">
+      <div className="z-[2] relative pt-4 px-6 pb-0">
         <div className="flex gap-4">
           <div>
             <GradientBorder className="rounded-full p-[2px]">
               <div className="bg-black rounded-full p-[4px]">
                 <Image
-                  src={'/person3.jpg'}
+                  src={"/person3.jpg"}
                   width={500}
                   height={500}
-                  className="size-[60px] object-cover object-top rounded-full"
+                  className="size-[84px] object-cover object-top rounded-full"
                   placeholder={`data:image/svg+xml;base64,${toBase64(
-                    shimmer(1200, 1800),
+                    shimmer(1200, 1800)
                   )}`}
                   alt="DP"
                 />
@@ -57,16 +56,18 @@ const ProfileSidebar = ({
             </GradientBorder>
           </div>
           <div>
-            <p className="text-primary text-sm font-bold">TOP USER</p>
-            <p className="font-bold text-lg">Sohail Hussain</p>
+            <p className="text-primary text-sm font-bold pt-[5px]">TOP USER</p>
+            <p className="font-bold text-lg pt-[2px] pb-[8px]">
+              Sohail Hussain
+            </p>
             <p className="text-sm">
-              <span className="text-[#E6E6E6]">07 Attend</span>{' '}
-              <span className="opacity-20">|</span>{' '}
+              <span className="text-[#E6E6E6]">07 Attend</span>{" "}
+              <span className="opacity-20">|</span>{" "}
               <span className="text-[#E6E6E6]">324 Following</span>
             </p>
           </div>
         </div>
-        <div className="mt-8 flex flex-col gap-1.5">
+        {/* <div className="mt-8 flex flex-col gap-1.5">
           <AccountSidebarLink
             setPopupOpen={setPopupOpen}
             title="Your Tickets"
@@ -79,10 +80,10 @@ const ProfileSidebar = ({
             leftElement={<Trophy size={20} weight="bold" />}
             url="/profile/your-rewards"
           />
-        </div>
-        <div className="mt-4">
+        </div> */}
+        <div className="mt-8">
           <p className="text-muted font-black">SETTINGS</p>
-          <div className="mt-2 flex flex-col gap-1.5">
+          <div className="mt-2 flex flex-col gap-2">
             <AccountSidebarLink
               setPopupOpen={setPopupOpen}
               title="Account Settings"
@@ -91,15 +92,15 @@ const ProfileSidebar = ({
             />
             <AccountSidebarLink
               setPopupOpen={setPopupOpen}
-              title="Languages"
-              leftElement={<Translate size={20} weight="bold" />}
+              title="Live Activity Settings"
+              leftElement={<Chats size={20} weight="bold" />}
               url="/profile/languages"
             />
           </div>
         </div>
-        <div className="mt-4">
+        <div className="mt-5">
           <p className="text-muted font-black">SUPPORTS</p>
-          <div className="mt-2 flex flex-col gap-1.5">
+          <div className="mt-3 flex flex-col gap-2">
             <AccountSidebarLink
               setPopupOpen={setPopupOpen}
               title="Help Center"
@@ -108,7 +109,7 @@ const ProfileSidebar = ({
             />
             <AccountSidebarLink
               setPopupOpen={setPopupOpen}
-              title="FAQS"
+              title="FAQs"
               leftElement={<Question size={20} weight="bold" />}
               url="/profile/faqs"
             />
@@ -116,7 +117,7 @@ const ProfileSidebar = ({
         </div>
         <div className="mt-4">
           <p className="text-muted font-black">ABOUT</p>
-          <div className="mt-2 flex flex-col gap-1.5">
+          <div className="mt-2 flex flex-col pb-5 gap-2">
             <AccountSidebarLink
               setPopupOpen={setPopupOpen}
               title="Terms & Conditions"
@@ -129,8 +130,18 @@ const ProfileSidebar = ({
               leftElement={<FileLock size={20} weight="bold" />}
               url="/profile/privacy-policy"
             />
+    {/* <AccountSidebarLink
+      setPopupOpen={setPopupOpen}
+      title="Delete Account"
+      leftElement={<Deleteaccnt size={20} weight="bold" />}
+      url="/profile/privacy-policy"
+    /> */}
+
           </div>
         </div>
+        <button className="bg-customRed w-full h-full border-none mb-[23.76px] rounded-full py-4 ">
+          logout
+        </button>
       </div>
     </div>
   );
