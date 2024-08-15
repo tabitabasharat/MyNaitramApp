0831a14a3e41745a15c0284c8345f7890ef704d6
tabitabasharat <tabitab632@gmail.com>
2024-08-15 21:19:30 +0500

    pagination

--- src/components/reusable-components/AllEventsGrid.tsx
+++ src/components/reusable-components/AllEventsGrid.tsx
@@ -5,20 +5,20 @@ import { Pagination } from "swiper/modules";
 const AllEventsGrid = ({ events }: any) => {
   return (
     <>
-    <div className="relative grid md:grid-cols-2 lg:grid-cols-3 gap-[1rem]">
-      {events.map((event: any) => (
-        <EventCard key={event.id} img={event.img} title={event.title} />
-      ))}
+      <div className="relative grid md:grid-cols-2 lg:grid-cols-3 gap-[1rem]">
+        {events.map((event: any) => (
+          <EventCard key={event.id} img={event.img} title={event.title} />
+        ))}
 
-      <div
-        // style={{
-        //   background:
-        //     "linear-gradient(to top, black, transparent 33%, transparent 66%, transparent)",
-        // }}
-        className="absolute inset-0 to-transparent z-[3] pointer-events-none"
-      ></div>
- 
-      {/* <FadeReveal extraStyle="z-[4]">
+        <div
+          // style={{
+          //   background:
+          //     "linear-gradient(to top, black, transparent 33%, transparent 66%, transparent)",
+          // }}
+          className="absolute inset-0 to-transparent z-[3] pointer-events-none"
+        ></div>
+
+        {/* <FadeReveal extraStyle="z-[4]">
         <Button
           variant="secondary"
           className="w-fit absolute bottom-[5%] left-1/2 -translate-x-1/2"
@@ -26,35 +26,42 @@ const AllEventsGrid = ({ events }: any) => {
           View More Events
         </Button>
       </FadeReveal> */}
-    </div>
-         <nav aria-label="Page navigation example">
-         <ul className="pagination justify-content-end">
-           <li className="page-item disabled">
-             <a className="page-link">Previous</a>
-           </li>
-           <li className="page-item">
-             <a className="page-link" href="#">
-               1
-             </a>
-           </li>
-           <li className="page-item">
-             <a className="page-link" href="#">
-               2
-             </a>
-           </li>
-           <li className="page-item">
-             <a className="page-link" href="#">
-               3
-             </a>
-           </li>
-           <li className="page-item">
-             <a className="page-link" href="#">
-               Next
-             </a>
-           </li>
-         </ul>
-       </nav>
-       </>
+      </div>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination pt-[32px] flex justify-between">
+          <li className="page-item disabled">
+            <a className="page-link bg-[#424242] p-[16px] text-base font-bold rounded-[8px]">Previous</a>
+          </li>
+          <div className="flex">
+            <li className="page-item">
+              <a className="page-link text-sm font-normal text-[#D9D9D9]" href="#">
+                page
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link text-sm font-normal text-[#D9D9D9]" href="#">
+                1
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link text-sm font-normal text-[#D9D9D9]" href="#">
+             {" "}   of
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link text-sm font-normal text-[#D9D9D9]" href="#">
+                8
+              </a>
+            </li>
+          </div>
+          <li className="page-item">
+            <a className="page-link bg-[white] text-[black] p-[16px] text-base font-bold rounded-[8px]" href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
--- src/components/reusable-components/MobileAllEventsList.tsx
+++ src/components/reusable-components/MobileAllEventsList.tsx
@@ -4,25 +4,49 @@ import MobileEventCard from './MobileEventCard';
 
 const MobileAllEventsList = ({ events }: any) => {
   return (
-    <div className="relative flex flex-col gap-4">
+    <div className="relative flex flex-col gap-[12px]">
       <MobileFilter />
       {events.slice(0, 6).map((event: any) => (
         <MobileEventCard key={event.id} img={event.img} title={event.title} />
       ))}
 
       <div
-        style={{
-          background:
-            'linear-gradient(to top, black, transparent 33%, transparent 66%, transparent)',
-        }}
         className="absolute inset-0 to-transparent z-[3] pointer-events-none"
       ></div>
-      <Button
-        variant="secondary"
-        className="z-[4] w-fit absolute bottom-[5%] left-1/2 -translate-x-1/2"
-      >
-        View More Events
-      </Button>
+      <nav aria-label="Page navigation example">
+        <ul className="pagination mt-[28px] flex justify-between">
+          <li className="page-item disabled">
+            <a className="page-link bg-[#424242] p-[16px] text-base font-bold rounded-[8px]">Previous</a>
+          </li>
+          <div className="flex">
+            <li className="page-item">
+              <a className="page-link text-sm font-normal text-[#D9D9D9]" href="#">
+                page
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link text-sm font-normal text-[#D9D9D9]" href="#">
+                1
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link text-sm font-normal text-[#D9D9D9]" href="#">
+             {" "}   of
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link text-sm font-normal text-[#D9D9D9]" href="#">
+                8
+              </a>
+            </li>
+          </div>
+          <li className="page-item">
+            <a className="page-link bg-[white] text-[black] p-[16px] text-base font-bold rounded-[8px]" href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
