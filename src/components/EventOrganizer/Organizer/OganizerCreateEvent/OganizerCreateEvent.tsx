8015dee657ef3bcec4bdff0eb4b181ec7d3ebd4a
abuhuraira1234567889 <hurairaabu098@gmail.com>
2024-09-14 15:59:34 +0500

    update

--- src/components/EventOrganizer/Organizer/OganizerCreateEvent/OganizerCreateEvent.tsx
+++ src/components/EventOrganizer/Organizer/OganizerCreateEvent/OganizerCreateEvent.tsx
@@ -1280,6 +1280,7 @@ function OganizerCreateEvent() {
 
                               setEventdescription(content);
                             }}
+                            value={Eventdescription}
                           />
                         </div>
                       </FormControl>
