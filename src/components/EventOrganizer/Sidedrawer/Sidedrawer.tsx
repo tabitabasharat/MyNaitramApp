c96d51963330e52e75a982e811c7eb230fd5b31c
tabitabasharat <tabitab632@gmail.com>
2024-10-03 11:05:42 +0500

    scrollbar

--- src/components/EventOrganizer/Sidedrawer/Sidedrawer.tsx
+++ src/components/EventOrganizer/Sidedrawer/Sidedrawer.tsx
@@ -133,7 +133,7 @@ const Sidedrawer: React.FC<Props> = ({ window, children }) => {
 
   const drawer = (
     <>
-      <div className="ps-[24px] pe-[24px] bg-[black] scrolling-issue">
+      <div className="ps-[24px] pe-[24px] bg-[black] scrolling-issue ">
         <List className="bg-[black] p-[0px] text-[white]">
           <Link href="/organizer-event/event-dashboard">
             <h3 className="text-[20px] ps-[6px] font-bold pt-[0px] lg:pt-[32px]  mb-[24px]">
