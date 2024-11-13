a6bc533d0db6c48070b3375584f0f455e48f762c
Zeeshan <zz1692733@gmail.com>
2024-11-14 01:56:19 +0500

    .

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -15,11 +15,7 @@ import arrowup from "@/assets/Arrow up.svg";
 import arrowdown from "@/assets/aboutdropdown.svg";
 // import arrowDownIcon from "@/assets/arrow-down-drop.svg"
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import SignInModal from "@/components/auth/SignInModal";
 import SignUpModal from "@/components/auth/SignUpModal";
 
@@ -35,16 +31,8 @@ import { useRouter } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { showProfile } from "@/lib/middleware/profile";
 import bellred from "@/assets/Wallet/bell red.svg";
-import {
-  getUserNotifications,
-  getOrgNotifications,
-  UserNotificationReadAll,
-  OrgNotificationReadAll,
-} from "@/lib/middleware/notification";
-import {
-  SuccessToast,
-  ErrorToast,
-} from "../reusable-components/Toaster/Toaster";
+import { getUserNotifications, getOrgNotifications, UserNotificationReadAll, OrgNotificationReadAll } from "@/lib/middleware/notification";
+import { SuccessToast, ErrorToast } from "../reusable-components/Toaster/Toaster";
 const Header = () => {
   const router = useRouter();
   const [authMode, setAuthMode] = useState<AuthMode>("SIGNIN");
@@ -91,10 +79,7 @@ const Header = () => {
   // Click outside handler to close dropdown
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
         setOpenDropdown(null); // Close dropdown if click is outside
       }
     };
@@ -119,8 +104,7 @@ const Header = () => {
   };
 
   useEffect(() => {
-    const userid =
-      typeof window !== "undefined" ? localStorage.getItem("_id") : null;
+    const userid = typeof window !== "undefined" ? localStorage.getItem("_id") : null;
     console.log("user id ", userid);
     dispatch(getUserNotifications(userid));
     dispatch(getOrgNotifications(userid));
@@ -160,13 +144,11 @@ const Header = () => {
     { id: 5, title: "Get Sponsored", url: "/get-sponsor" },
   ];
   useEffect(() => {
-    const id =
-      typeof window !== "undefined" ? localStorage.getItem("_id") : null;
+    const id = typeof window !== "undefined" ? localStorage.getItem("_id") : null;
     setToken(id);
   }, [token, count]);
   useEffect(() => {
-    const userid =
-      typeof window !== "undefined" ? localStorage.getItem("_id") : null;
+    const userid = typeof window !== "undefined" ? localStorage.getItem("_id") : null;
     console.log("user id ", userid);
     dispatch(showProfile(userid));
   }, []);
@@ -177,9 +159,7 @@ const Header = () => {
     dispatch({ type: "LOGOUT" });
     router.push("/");
   };
-  const myProfile = useAppSelector(
-    (state) => state?.getShowProfile?.myProfile?.data
-  );
+  const myProfile = useAppSelector((state) => state?.getShowProfile?.myProfile?.data);
   // const [openDropdown, setOpenDropdown] = useState<number | null>(null);
 
   // const handleDropdownToggle = (id: number) => {
@@ -192,33 +172,24 @@ const Header = () => {
       console.log("Token host ", token);
       setIsLoginDialogOpen(true);
     } else {
-      const userid =
-        typeof window !== "undefined" ? localStorage.getItem("_id") : null;
+      const userid = typeof window !== "undefined" ? localStorage.getItem("_id") : null;
       console.log("No token ", token);
       router.push("/organizer-event/event-dashboard");
     }
   };
 
-  const Notify = useAppSelector(
-    (state) => state?.getUserNotifications?.myNotifications?.data
-  );
-  const NotifyOrg = useAppSelector(
-    (state) => state?.getOrgNotifications?.myNotifications?.data
-  );
-  const Unreadnotification =
-    Notify && Notify?.some((item: any) => item && item?.NotifyRead === false);
+  const Notify = useAppSelector((state) => state?.getUserNotifications?.myNotifications?.data);
+  const NotifyOrg = useAppSelector((state) => state?.getOrgNotifications?.myNotifications?.data);
+  const Unreadnotification = Notify && Notify?.some((item: any) => item && item?.NotifyRead === false);
 
-  const UnreadnotificationOrg =
-    NotifyOrg &&
-    NotifyOrg?.some((item: any) => item && item?.NotifyRead === false);
+  const UnreadnotificationOrg = NotifyOrg && NotifyOrg?.some((item: any) => item && item?.NotifyRead === false);
 
   console.log("Notify:", Notify);
   console.log("Unreadnotification:", Unreadnotification);
 
   async function UserReadAll(id: any) {
     console.log("my notify id is", id);
-    const userid =
-      typeof window !== "undefined" ? localStorage.getItem("_id") : null;
+    const userid = typeof window !== "undefined" ? localStorage.getItem("_id") : null;
     setLoader(true);
 
     try {
@@ -243,8 +214,7 @@ const Header = () => {
 
   async function OrgReadAll(id: any) {
     console.log("my notify id is", id);
-    const userid =
-      typeof window !== "undefined" ? localStorage.getItem("_id") : null;
+    const userid = typeof window !== "undefined" ? localStorage.getItem("_id") : null;
     setLoader(true);
 
     try {
@@ -270,13 +240,7 @@ const Header = () => {
   return (
     <>
       <AnimatePresence mode="wait">
-        <Menu
-          authMode={authMode}
-          setAuthMode={setAuthMode}
-          menuIsOpen={menuIsOpen}
-          setMenuIsOpen={setMenuIsOpen}
-          toggleMenu={toggleMenu}
-        />
+        <Menu authMode={authMode} setAuthMode={setAuthMode} menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} toggleMenu={toggleMenu} />
       </AnimatePresence>
       <AnimatePresence mode="wait">
         {menuIsOpen && (
@@ -291,10 +255,9 @@ const Header = () => {
         )}
       </AnimatePresence>
       <header
-        className={cn(
-          "fixed w-full pxpx py-[26px] sm:pr-[24px] flex items-center justify-between z-50 duration-300",
-          { "bg-black/50 backdrop-blur-lg webkit-header-blur": fixedBg }
-        )}
+        className={cn("fixed w-full pxpx py-[26px] sm:pr-[24px] flex items-center justify-between z-50 duration-300", {
+          "bg-black/50 backdrop-blur-lg webkit-header-blur": fixedBg,
+        })}
       >
         <Link href="/">
           <div className="">
@@ -303,18 +266,10 @@ const Header = () => {
         </Link>
         <nav className="nav-inside">
           {links.map((link) => (
-            <div
-              key={link.id}
-              className="relative group"
-              ref={link.id === openDropdown ? dropdownRef : null}
-            >
+            <div key={link.id} className="relative group" ref={link.id === openDropdown ? dropdownRef : null}>
               {/* Main Link */}
               <div className="flex items-center cursor-pointer">
-                <Link
-                  href={link.url}
-                  className={`text-base font-normal ${pathname === link.url ? "active" : ""
-                    }`}
-                >
+                <Link href={link.url} className={`text-base font-normal ${pathname === link.url ? "active" : ""}`}>
                   {link.title}
                 </Link>
 
@@ -341,11 +296,7 @@ const Header = () => {
               {link.subLinks && openDropdown === link.id && (
                 <div className="absolute left-0 mt-2 text-base font-bold gradient-slate bg-white shadow-lg rounded-md z-10">
                   {link.subLinks.map((subLink, j) => (
-                    <Link
-                      key={j}
-                      href={subLink.url}
-                      className="block px-4 py-2 hover:gradient-slate active:text-[#00D059] active:gradient-slate"
-                    >
+                    <Link key={j} href={subLink.url} className="block px-4 py-2 hover:gradient-slate active:text-[#00D059] active:gradient-slate">
                       {subLink.title}
                     </Link>
                   ))}
@@ -364,10 +315,7 @@ const Header = () => {
               className="flex items-center add-bank-account-border  bg-black gap-[4px] p-[12px]"
             >
               <Image src={greenticket} alt="greenticket" />
-              <p className=" font-extrabold text-base text-[#00D059]">
-                {" "}
-                Verify Ticket
-              </p>
+              <p className=" font-extrabold text-base text-[#00D059]"> Verify Ticket</p>
             </Button>
           </div>
           <div>
@@ -380,31 +328,16 @@ const Header = () => {
           </div>
           <>
             {!token && (
-              <Dialog
-                open={isLoginDialogOpen}
-                onOpenChange={setIsLoginDialogOpen}
-              >
+              <Dialog open={isLoginDialogOpen} onOpenChange={setIsLoginDialogOpen}>
                 <DialogTrigger asChild>
-                  <Button
-                    variant="secondary"
-                    className="hidden pb-[6px] lg:block"
-                  >
+                  <Button variant="secondary" className="hidden pb-[6px] lg:block">
                     Sign In
                   </Button>
                 </DialogTrigger>
                 {authMode === "SIGNIN" && isLoginDialogOpen && (
-                  <SignInModal
-                    redirectRoute={`/viewallevents`}
-                    setAuthMode={setAuthMode}
-                    setSigninModal={() => setIsLoginDialogOpen(false)}
-                  />
+                  <SignInModal redirectRoute={`/viewallevents`} setAuthMode={setAuthMode} setSigninModal={() => setIsLoginDialogOpen(false)} />
                 )}
-                {authMode === "SIGNUP" && (
-                  <SignUpModal
-                    setAuthMode={setAuthMode}
-                    setSigninModal={() => setIsLoginDialogOpen(false)}
-                  />
-                )}
+                {authMode === "SIGNUP" && <SignUpModal setAuthMode={setAuthMode} setSigninModal={() => setIsLoginDialogOpen(false)} />}
               </Dialog>
             )}
           </>
@@ -412,10 +345,7 @@ const Header = () => {
           {isLoggedIn && token && (
             <div className="relative mr-[12px] md:mr-[12px] lg:ms-0 lg:mr-[12px] xl:mr-- flex items-center lg:gap-[20px] gap-[10px] h-full cursor-pointer">
               <Popover open={notifPopupOpen} onOpenChange={setNotifPopupOpen}>
-                <PopoverTrigger
-                  asChild
-                  className="relative z-[1200] cursor-pointer"
-                >
+                <PopoverTrigger asChild className="relative z-[1200] cursor-pointer">
                   {Unreadnotification || UnreadnotificationOrg ? (
                     <Image
                       className="lg:size-[24px] h-[28px] w-[28px] lg:h-[24px] lg:w-[24px] size-{28px} cursot-pointer"
@@ -432,11 +362,7 @@ const Header = () => {
                 shadow-custom bg-black w-[350px] lg:w-[400px] rounded-2xl  -translate-x-4 translate-y-6"
                 >
                   <ScrollArea className="h-[600px] px-[8px] pb-[15px] border-none w-full ">
-                    <NotificationPopUp
-                      setNotifPopupOpen={setNotifPopupOpen}
-                      activeTab={activeTab}
-                      setActiveTab={setActiveTab}
-                    />
+                    <NotificationPopUp setNotifPopupOpen={setNotifPopupOpen} activeTab={activeTab} setActiveTab={setActiveTab} />
                   </ScrollArea>
                   {activeTab == "USER" && Notify && Unreadnotification && (
                     <Button
@@ -467,41 +393,26 @@ const Header = () => {
                     {/* <div className="border h-[44px] w-[44px] border-muted gradient-slate rounded-full items-center  cursor-pointer"> */}
                     <div className="size-[44px] lg:size-[44px] gradient-slate p-[6px] rounded-full overflow-hidden  shadow-inner shadow-md border border-gray-700 rounded-full border-gradient bg-gradient-to-t from-transparent to-transparent">
                       <div>
-                        <Link
-                          href={"/profile/profile-main"}
-                          className="display-none"
-                        >
+                        <Link href={"/profile/profile-main"} className="display-none">
                           <Image
-                            src={
-                              myProfile?.profilePicture
-                                ? myProfile?.profilePicture
-                                : "/person3.jpg"
-                            }
+                            src={myProfile?.profilePicture ? myProfile?.profilePicture : "/person3.jpg"}
                             width={32}
                             height={32}
                             className="object-cover object-center rounded-full w-[32px] h-[32px]"
-                            placeholder={`data:image/svg+xml;base64,${toBase64(
-                              shimmer(1200, 1800)
-                            )}`}
+                            placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(1200, 1800))}`}
                             alt="DP"
                           />
                         </Link>
                       </div>
 
                       {/* </div> */}
-
                     </div>
                     <div className="relative inline-block text-left" ref={dropdownRef}>
                       {/* Dropdown Button */}
                       <div>
-                        <button
-                          onClick={toggleDropdown}
-                          className=""
-                        >
-
+                        <button onClick={toggleDropdown} className="">
                           <svg
-                            className={` h-[20px] w-[20px] text-gray-400 transition-transform duration-200 ${isOpen ? "rotate-180" : ""
-                              }`}
+                            className={` h-[20px] w-[20px] text-gray-400 transition-transform duration-200 ${isOpen ? "rotate-180" : ""}`}
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
@@ -537,16 +448,14 @@ const Header = () => {
                               <Link
                                 href="/organizer-event/event-dashboard"
                                 onClick={() => handleLinkClick("Organizer Profile")}
-                                className={`block text-start text-sm ${activeLink === "Organizer Profile" ? "text-green-500" : "text-white"
-                                  }`}
+                                className={`block text-start text-sm ${activeLink === "Organizer Profile" ? "text-green-500" : "text-white"}`}
                               >
                                 Organizer Profile
                               </Link>
                               <Link
                                 href="/profile/profile-main"
                                 onClick={() => handleLinkClick("User Profile")}
-                                className={`block text-start text-sm ${activeLink === "User Profile" ? "text-green-500" : "text-white"
-                                  }`}
+                                className={`block text-start text-sm ${activeLink === "User Profile" ? "text-green-500" : "text-white"}`}
                               >
                                 User Profile
                               </Link>
@@ -556,7 +465,6 @@ const Header = () => {
                       </div>
                     </div>
                   </div>
-
                 </PopoverTrigger>
                 <PopoverContent className="bg-black rounded-2xl text-white border-none shadow-none p-0 -translate-x-4 translate-y-2">
                   <ScrollArea className="h-[550px] rounded-2xl shadow-custom">
@@ -570,11 +478,7 @@ const Header = () => {
             </div>
           )}
 
-          <button
-            type="button"
-            onClick={toggleMenu}
-            className="flex translate-x-[0rem] items-center gap-[0.1rem] menu-show "
-          >
+          <button type="button" onClick={toggleMenu} className="flex translate-x-[0rem] items-center gap-[0.1rem] menu-show ">
             <div className="text-black bg-white rounded-full size-10 grid place-items-center">
               <div className="translate-x-[-0.3rem] translate-y-[-0.2rem]">
                 <Hamburger toggled={menuIsOpen} size={20} />
