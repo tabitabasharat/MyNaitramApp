4d443dfa60d544bc450821b9a1f83706d7ca604b
Zeeshan <zz1692733@gmail.com>
2024-11-14 18:23:18 +0500

    .

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,7 +1,7 @@
 // export const API_URL = "https://pocketfiler.blockmob.io"
 // export const API_URL = "https://naitrambackend.techsolutions.london"
 
-// export const API_URL = "https://backend.naitram.live";
+export const API_URL = "https://backend.naitram.live";
 // export const API_URL = "http://192.168.18.42:3001"
 // export const API_URL = "http://ec2-35-153-184-112.compute-1.amazonaws.com:3001"
 
@@ -11,6 +11,6 @@
 // export const API_URL ="https://staggingbe.blockmoblabs.com"
 
 // export const API_URL = "https://staggingbe.blockmoblabs.com";
-export const API_URL = "https://stagingbe.dotskulls.com";
+// export const API_URL = "https://stagingbe.dotskulls.com";
 
 // export const API_URL = "https://something.dotskulls.com";
