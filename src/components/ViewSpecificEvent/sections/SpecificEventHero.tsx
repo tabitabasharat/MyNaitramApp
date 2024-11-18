aca4d7693f8ad7fe9baa741cdb336489e9e5462d
Zeeshan <zz1692733@gmail.com>
2024-11-18 14:30:15 +0500

    .

--- src/components/ViewSpecificEvent/sections/SpecificEventHero.tsx
+++ src/components/ViewSpecificEvent/sections/SpecificEventHero.tsx
@@ -322,7 +322,6 @@ const SpecificEventHero = ({ setShowTicket, eventType }: any) => {
                     </ul>
                   </div>
                 )}
-<<<<<<< HEAD
                 {/* {isModalOpen && (
                  <Report
                  open={isModalOpen} onClose={() => setIsModalOpen(false)}
@@ -334,9 +333,6 @@ const SpecificEventHero = ({ setShowTicket, eventType }: any) => {
 
               {/* Add a Feedback Modal here if needed */}
               {isModalOpen && modalContent === "Feedback" && <FeedbackModal open={isModalOpen} onClose={() => setIsModalOpen(false)} />}
-=======
-              </div> */}
->>>>>>> a77c72968697e9a78a41c409bfd531bed49a042a
             </div>
             {sharemodal && <ShareModal onClose={() => setShareModal(false)} open={() => setShareModal(true)} eventUrl={copiedUrl} />}
           </div>
