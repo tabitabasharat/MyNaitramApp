2a8178e7575a2bed9cb99715b16cde6d363de16f
Zeeshan <zz1692733@gmail.com>
2024-11-20 17:02:57 +0500

    QA fixes

--- src/components/notifications/EventNotificationCard.tsx
+++ src/components/notifications/EventNotificationCard.tsx
@@ -1,36 +1,17 @@
 import { shimmer, toBase64 } from "@/lib/utils";
 import Image from "next/image";
 import { useAppSelector, useAppDispatch } from "@/lib/hooks";
-import {
-  getUserNotifications,
-  getOrgNotifications,
-  notificationStatus,
-  notificationStatusOrg,
-} from "@/lib/middleware/notification";
+import { getUserNotifications, getOrgNotifications, notificationStatus, notificationStatusOrg } from "@/lib/middleware/notification";
 import { useState, useEffect } from "react";
-import {
-  SuccessToast,
-  ErrorToast,
-} from "../reusable-components/Toaster/Toaster";
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import { SuccessToast, ErrorToast } from "../reusable-components/Toaster/Toaster";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
 
-const EventNotificationCard = ({
-  msg,
-  heading,
-  notifyTime,
-  profileimg,
-  readStatus,
-  notificationId,
-  notifyType,
-
-}: any) => {
+const EventNotificationCard = ({ msg, heading, notifyTime, profileimg, readStatus, notificationId, notifyType }: any) => {
   const dispatch = useAppDispatch();
   const [loader, setLoader] = useState(false);
-  const Notify = useAppSelector(
-    (state) => state?.getUserNotifications?.myNotifications?.data
-  );
+  const Notify = useAppSelector((state) => state?.getUserNotifications?.myNotifications?.data);
 
   console.log("All Notifications are", Notify);
   const formatTimeol = (isoString: string): string => {
@@ -43,26 +24,25 @@ const EventNotificationCard = ({
   };
 
   // const formatTimeAGo = (isoString: string): string => {
-  //   return dayjs(isoString).fromNow(); 
+  //   return dayjs(isoString).fromNow();
   // };
 
   const formatTime = (isoString: string): string => {
     const notificationTime = dayjs(isoString);
     const now = dayjs();
-  
+
     // Check if the notification is today
-    if (notificationTime.isSame(now, 'day')) {
-      return notificationTime.format('h:mm A'); // Format to show hours and minutes (e.g., 3:45 PM)
-    } else if (notificationTime.isSame(now.subtract(1, 'day'), 'day')) {
-      return 'Yesterday'; 
+    if (notificationTime.isSame(now, "day")) {
+      return notificationTime.format("h:mm A"); // Format to show hours and minutes (e.g., 3:45 PM)
+    } else if (notificationTime.isSame(now.subtract(1, "day"), "day")) {
+      return "Yesterday";
     } else {
       return notificationTime.fromNow(); // Use relative time for older notifications
     }
   };
   async function readnotification(id: any) {
     console.log("my notify id is", id);
-    const userid =
-      typeof window !== "undefined" ? localStorage.getItem("_id") : null;
+    const userid = typeof window !== "undefined" ? localStorage.getItem("_id") : null;
     setLoader(true);
 
     try {
@@ -87,8 +67,7 @@ const EventNotificationCard = ({
 
   async function readOrgnotification(id: any) {
     console.log("my notify id is", id);
-    const userid =
-      typeof window !== "undefined" ? localStorage.getItem("_id") : null;
+    const userid = typeof window !== "undefined" ? localStorage.getItem("_id") : null;
     setLoader(true);
 
     try {
@@ -121,16 +100,14 @@ const EventNotificationCard = ({
   };
 
   return (
-    <div className="gradient-slate border border-muted w-full rounded-lg p-3 flex gap-4">
-      <div className="size-[60px] rounded-lg border border-white overflow-hidden">
+    <div className="gradient-slate border border-muted w-full rounded-lg p-3 flex gap-3">
+      <div className="w-[56px] h-[48px] rounded-[30px] border border-white overflow-hidden">
         <Image
-          src={profileimg !== null ? profileimg : "/person3.jpg"}
+          src={profileimg !== null ? profileimg : "/person3TrnsBG.png"}
           width={500}
           height={500}
           className="object-cover size-[60px]"
-          placeholder={`data:image/svg+xml;base64,${toBase64(
-            shimmer(1200, 1800)
-          )}`}
+          placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(1200, 1800))}`}
           alt=""
         />
       </div>
@@ -157,10 +134,7 @@ const EventNotificationCard = ({
           {msg}
         </p>
         {!readStatus && (
-          <p
-            className="text-[#00D059] text-end text-[12px] cursor-pointer hover:underline"
-            onClick={handleRead}
-          >
+          <p className="text-[#00D059] text-end text-[12px] cursor-pointer hover:underline" onClick={handleRead}>
             Mark as read
           </p>
         )}
